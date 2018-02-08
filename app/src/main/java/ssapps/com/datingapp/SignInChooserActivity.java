2cd37961da17d6cd6cdc46150a3679f4bf7b682a
sagar1729sagar <ssapps.sagar@gmail.com>
2018-02-09 02:45:25 +0530

    Signup screen inflated

--- app/src/main/java/ssapps/com/datingapp/SignInChooserActivity.java
+++ app/src/main/java/ssapps/com/datingapp/SignInChooserActivity.java
@@ -1,13 +1,31 @@
 package ssapps.com.datingapp;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import ssapps.com.datingapp.databinding.ActivitySignInChooserBinding;
 
 public class SignInChooserActivity extends AppCompatActivity {
+
+    ActivitySignInChooserBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_chooser);
+       // setContentView(R.layout.activity_sign_in_chooser);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_in_chooser);
+
+        binding.registerChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // startActivityForResult(new Intent(SignInChooserActivity.this,SignupActivity.class),0);
+                startActivity(new Intent(SignInChooserActivity.this,SignupActivity.class));
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/ssapps/com/datingapp/SignupActivity.java
@@ -0,0 +1,36 @@
+package ssapps.com.datingapp;
+
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
