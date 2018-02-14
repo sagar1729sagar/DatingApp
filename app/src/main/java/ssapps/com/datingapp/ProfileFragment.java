fc08de986e95708f2feb1313f2c7e3151805539a
VIDYA SAGAR <sagar1729sagar@gmail.com>
2018-02-14 14:54:36 +0530

    profilepage created

---
+++ app/src/main/java/ssapps/com/datingapp/ProfileFragment.java
@@ -0,0 +1,13 @@
+package ssapps.com.datingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_fragment);
+    }
+}
