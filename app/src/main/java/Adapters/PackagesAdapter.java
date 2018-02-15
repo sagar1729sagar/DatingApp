dec42fcb7afbfc6883ed4b8d7d7e722d41318d80
VIDYA SAGAR <sagar1729sagar@gmail.com>
2018-02-15 17:04:47 +0530

    packages in progress 1

--- app/src/main/java/Adapters/PackagesAdapter.java
+++ app/src/main/java/Adapters/PackagesAdapter.java
@@ -1,4 +1,18 @@
 package Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class PackagesAdapter {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView title,price,p0,p1,p2,p3,p4,p5,p6,p7,p8,p9;
+        public ImageView arrow;
+        public Button orderButton;
+
+    }
+
 }
