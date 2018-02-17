83bd1baa9fb375524ecacf1f1b67390298c40e8e
VIDYA SAGAR <sagar1729sagar@gmail.com>
2018-02-17 13:03:25 +0530

    Search results displayed

--- app/src/main/java/ssapps/com/datingapp/SearchResultsDisplayActivity.java
+++ app/src/main/java/ssapps/com/datingapp/SearchResultsDisplayActivity.java
@@ -1,22 +1,45 @@
 package ssapps.com.datingapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import Adapters.SearchResultsAdapter;
+import Models.SearchResults;
+import ssapps.com.datingapp.databinding.ActivitySearchResultsDisplayBinding;
 
 public class SearchResultsDisplayActivity extends AppCompatActivity {
+
+    private List<SearchResults> results;
+    private SearchResultsAdapter adapter;
+    private ActivitySearchResultsDisplayBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results_display);
-
+        //setContentView(R.layout.activity_search_results_display);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_search_results_display);
 
         ActionBar actionBar = getSupportActionBar();
 
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        results = SearchResults.listAll(SearchResults.class);
+        adapter = new SearchResultsAdapter(this,results);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.searchResultsRecyclerView.setLayoutManager(layoutManager);
+        binding.searchResultsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        binding.searchResultsRecyclerView.setAdapter(adapter);
+
+        //todo row click to be done
 
     }
 }
