08b3127b83c8bad3154ef6e2b1d091d9b8c5a961
VIDYA SAGAR <sagar1729sagar@gmail.com>
2018-02-17 12:54:28 +0530

    Search results display adapter created

---
+++ app/src/main/java/Adapters/SearchResultsAdapter.java
@@ -0,0 +1,95 @@
+package Adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Models.SearchResults;
+
+public class SearchResultsAdapter  extends RecyclerView.Adapter<SearchResultsAdapter.MyViewHolder>{
+
+    private List<SearchResults> results = new ArrayList<>();
+    private Context context;
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout., parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        SearchResults result = results.get(position);
+        Picasso.with(context).load(result.getPhotourl()).transform(new CropSquareTransformation())
+                                .into(holder.profile_image);
+
+        if (result.getIsOnline().equals("Yes")){
+            holder.online_offline_image.setImageDrawable(R.drawable.ic_online_dot);
+            holder.online_offilne_text.setText(context.getResources().getString(R.string.online));
+        } else if (result.getIsOnline().equals("No")){
+            holder.online_offline_image.setImageDrawable(R.drawable.ic_offline_dot);
+            holder.online_offilne_text.setText(context.getResources().getString(R.string.offline));
+        }
+
+        holder.details.setText(result.getUsername()+","+result.getAge_self());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public ImageView profile_image, online_offline_image,dummy;
+        public TextView details,online_offilne_text;
+
+        public MyViewHolder(View view){
+            super(view);
+            profile_image = (ImageView)view.findViewById(R.id.circle_image);
+            online_offline_image = (ImageView)view.findViewById(R.id.online_offline_image);
+            details = (TextView)view.findViewById(R.id.name_age_tv);
+            online_offilne_text = (TextView)view.findViewById(R.id.online_offline_tv);
+            dummy = (ImageView)view.findViewById(R.id.dummy_image);
+        }
+    }
+
+   public SearchResultsAdapter(Context context,List<SearchResults> results){
+        this.context = context;
+        this.results = results;
+   }
+
+    public class CropSquareTransformation implements Transformation {
+        @Override public Bitmap transform(Bitmap source) {
+
+            RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(context.getResources(),source);
+            dr.setCornerRadius(Math.max(source.getWidth(),source.getHeight()) / 2.0f);
+
+            return dr.getBitmap();
+
+        }
+
+        @Override public String key() { return "square()"; }
+    }
+
+}
--- app/src/main/java/ssapps/com/datingapp/SearchResultsDisplayActivity.java
+++ app/src/main/java/ssapps/com/datingapp/SearchResultsDisplayActivity.java
@@ -1,5 +1,6 @@
 package ssapps.com.datingapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class SearchResultsDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results_display);
+
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
     }
 }
